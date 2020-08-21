121b82e62730623ced52ab076cf8e2f839e3a846
MingYoung1999 <48091099+MingYoung1999@users.noreply.github.com>
2020-08-21 15:58:46 +0800

    更改首頁path

--- components/layouts/top.vue
+++ components/layouts/top.vue
@@ -2,7 +2,7 @@
     <div class="con">
         <div style="font-size: 18px;">
             <Breadcrumb style="margin:0 16px;line-height:64px;">
-                <BreadcrumbItem :to="'/'+this.$route.params.site+'/'" class="pointer">
+                <BreadcrumbItem to="/" class="pointer">
                     <Icon type="md-home" size="22" style="line-height:60px;padding-bottom:5px"/>
                 </BreadcrumbItem>
                 <BreadcrumbItem style="font-size:18px;">{{$route.name}}</BreadcrumbItem>
