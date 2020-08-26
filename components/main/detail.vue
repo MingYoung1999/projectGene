72c66591779cbd898c84908f2df7b85d4588f205
MingYoung1999 <48091099+MingYoung1999@users.noreply.github.com>
2020-08-26 15:45:08 +0800

    新增用藥紀錄

--- components/main/detail.vue
+++ components/main/detail.vue
@@ -7,11 +7,24 @@
                 size="small"
                 @click="onClickBack">返回</Button>
         </Row>
+        <Tabs>
+            <TabPane label="詳細資料">詳細資料</TabPane>
+            <TabPane label="用藥紀錄">用藥紀錄</TabPane>
+        </Tabs>
     </div>
 </template>
 
 <script>
 export default {
+    props: {
+        data: {
+            type: Object,
+            required: true,
+            default: () => {
+                return {}
+            }
+        },
+    },
     methods: {
         onClickBack(){
             this.$emit('onClickBack');
