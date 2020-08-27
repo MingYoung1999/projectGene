b5266394dd9a12d174ddce4433a4e96dcab66be4
MingYoung1999 <48091099+MingYoung1999@users.noreply.github.com>
2020-08-27 16:16:50 +0800

    新增用藥紀錄頁面

--- components/main/record.vue
+++ components/main/record.vue
@@ -1,10 +1,47 @@
 <template>
-
+    <div>
+        <iTable
+            :columns="columns"/>
+    </div>
 </template>
 
 <script>
+import iTable from '@/components/iTable'
 export default {
-
+    components: {
+        iTable,
+    },
+    data(){
+        return{
+            columns:[
+                {
+                    title: '日期',
+                    align: 'center',
+                    sortable: true,
+                },
+                {
+                    title: '醫院別',
+                    align: 'center',
+                    sortable: false,
+                },
+                {
+                    title: '藥名/劑量',
+                    align: 'center',
+                    sortable: false,
+                },
+                {
+                    title: '用法',
+                    align: 'center',
+                    sortable: false,
+                },
+                {
+                    title: '備註',
+                    align: 'center',
+                    sortable: false,
+                },
+            ],
+        }
+    },
 }
 </script>
 
