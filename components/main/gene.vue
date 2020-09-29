f5d3fc7f16bada71b61b617779005de561e72b1e
MingYoung1999 <48091099+MingYoung1999@users.noreply.github.com>
2020-09-29 16:27:57 +0800

    Update gene.vue

--- components/main/gene.vue
+++ components/main/gene.vue
@@ -12,37 +12,70 @@ export default {
             default: () => { return {} },
         },
     },
-    mounted() {
-        d3.select('body')
-            .append('svg')
-            .text(function(d){
-                // var list = [];
-                // list.push(
-                // {
-                //     chr: "11",
-                //     position: 118894558
-                // },
-                // {
-                //     chr:"19",
-                //     position:10005699
-                // },
-                // {
-                //     chr:"19",
-                //     position:52384651
-                // },)
-                var legend = [{
-                    name: 'Significance',
-                    rows: [
-                        {name: 'Mismatch', color: '#F00', shape: 'triangle'},
-                        {name: 'Match', color: '#8D4', shape: 'triangle'}
-                    ]
-                }];
-                var ideogram = new Ideogram({
+    data(){
+        const drawGene = ()=>{
+            var legend = [{
+                name: 'Significance',
+                rows: [
+                    {name: 'Mismatch', color: '#F00', shape: 'triangle'},
+                    {name: 'Match', color: '#8D4', shape: 'triangle'}
+                ]
+            }];
+            var ideogram = new Ideogram({
                 organism: 'human',
                 // annotations: list,
                 legend:legend
-                })
-            });
+            })
+        }
+    },
+    mounted() {
+        // d3.select('body')
+        //     .append('svg')
+        //     .text(function(d){
+        //         // var list = [];
+        //         // list.push(
+        //         // {
+        //         //     chr: "11",
+        //         //     position: 118894558
+        //         // },
+        //         // {
+        //         //     chr:"19",
+        //         //     position:10005699
+        //         // },
+        //         // {
+        //         //     chr:"19",
+        //         //     position:52384651
+        //         // },)
+        //         var legend = [{
+        //             name: 'Significance',
+        //             rows: [
+        //                 {name: 'Mismatch', color: '#F00', shape: 'triangle'},
+        //                 {name: 'Match', color: '#8D4', shape: 'triangle'}
+        //             ]
+        //         }];
+        //         var ideogram = new Ideogram({
+        //             organism: 'human',
+        //             // annotations: list,
+        //             legend:legend
+        //         })
+        //     });
+        d3.select('#ideo')
+        .append('svg')
+        .attr({
+        'width':200,
+        'height':200
+        });
+
+        d3.select('#ideo')
+        .append('circle')
+        .attr({
+        'cx':50,
+        'cy':50,
+        'r':30,
+        'fill':'#f90',
+        'stroke':'#c00',
+        'stroke-width':'5px'
+        });
     }
 }
 </script>
